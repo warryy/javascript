7cef1843a19d4d10dcc090e196065a6b62438cf2
warryy <1962851827@qq.com>
2023-03-16 17:23:46 +0800

    feat(mybind): done

--- handwritten-function/mybind.js
+++ handwritten-function/mybind.js
@@ -1,34 +1,32 @@
-// /**
-//  * 1. 绑定作用域
-//  * 2. 实现函数柯里化
-//  * 3. 返回的函数作为构造函数时, this 绑定失效
-//  */
-// function mybind(obj, ...args) {
-//   var that = this
-//   return function (innerArgs) {
-//     return that.call(obj, ...args, ...innerArgs)
-//   }
-// }
-
-// /**
-//  * obj = {a: 1}
-//  * var bound = fn.mybind(obj)
-//  * 不使用 new 的时候, bound 内部的 this 是指向 obj 的
-//  * 使用 new 的时候, this 失效, 依旧是指向 new 操作返回的那个对象
-//  *
-//  * new bound()
-//  *  1. 将返回的参数的原型指向 fn
-//  *  2. 将 fn 的原型指向 返回的参数
-//  *  3. 将 执行 fn 得到返回结果 res
-//  *    1. res !== null && (res 是 function 或着 object), 返回 res
-//  *    2. 返回 空的 obj
-//  */
 
 function fn(...args) {
   console.log(this.haha, ...args);
 }
 
-const newFn = fn.bind({ haha: "haha" }, 2, 3, 4);
-console.log(newFn());
+window.haha = "window haha";
 
+console.log(fn.bind({ haha: "haha" }, 2, 3, 4)(666));
 
+console.log(fn.bind(undefined, 2, 3, 4)(666));
+
+/**
+ * 自己实现
+ * 明确需求:
+ *  fn.bind(a, b, c)
+ *  将 fn 改为用 a 调用执行, 且绑定参数 b, c
+ * 边界情况
+ *  bind 参数为空
+ */
+Function.prototype.mybind = function (obj, ...args) {
+  if (typeof this !== "function") {
+    throw Error("mybind is not a function");
+  }
+  const that = this
+  return function (...myArgs) {
+    return that.call(obj || window, ...args, ...myArgs);
+  };
+};
+
+console.log(fn.mybind({ haha: "haha" }, 2, 3, 4)(666));
+
+console.log(fn.mybind(undefined, 2, 3, 4)(666));
